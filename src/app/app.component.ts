a36f5daef0215b3b4756c418b078eee7a9f9faf4
ychafia <chafiayouness@gmail.com>
2019-07-09 17:19:33 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,20 +23,22 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.resp.push({id: 1,month: 'Janvier-19', amount: 150});
-    this.resp.push({id: 2,month: 'Fevrier-19', amount: 150});
-    this.resp.push({id: 3,month: 'Mars-19', amount: 50});
+    this.resp.push({month: 'Janvier-19', amount: 150});
+    this.resp.push({month: 'Fevrier-19', amount: 150});
+    this.resp.push({month: 'Mars-19', amount: 50});
     console.log(this.resp.length);
     // console.log(this.months);
     let i = 1;
     for(let month of this.months){
       if(!this.exist(month)){
-        this.resp.push({id: i,month: month, amount: 0});
+        this.resp.push({month: month, amount: 0});
       }
       i++;
 
     }
     console.log(this.resp);
-    console.log(this.resp.sort());
+    for(let m of this.resp) {
+      console.log(m);
+    }
   }
 }
